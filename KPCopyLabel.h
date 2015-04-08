bfadbd218925d0266c1f39cbb1c705800096c6cc
huayu <huayu@huayudeiMac-2.local>
2015-04-08 16:49:02 +0800

    first init

---
+++ KPCopyLabel.h
@@ -0,0 +1,13 @@
+//
+//  KPCopyLabel.h
+//  KupingGame
+//
+//  Created by huayu on 15/4/8.
+//  Copyright (c) 2015年 ZhuJiaQuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KPCopyLabel : UILabel
+
+@end
---
+++ KPCopyLabel.m
@@ -0,0 +1,72 @@
+//
+//  KPCopyLabel.m
+//  KupingGame
+//
+//  Created by huayu on 15/4/8.
+//  Copyright (c) 2015年 ZhuJiaQuan. All rights reserved.
+//
+
+#import "KPCopyLabel.h"
+
+@implementation KPCopyLabel
+
+- (BOOL)canBecomeFirstResponder
+{
+    return YES;
+}
+
+- (BOOL)canPerformAction:(SEL)action withSender:(id)sender
+{
+    if (action == @selector(copyText:)) {
+        return YES;
+    }
+    return NO;
+}
+
+- (void)copyText:(id)sender
+{
+    UIPasteboard *pboard = [UIPasteboard generalPasteboard];
+    [pboard setString:self.text];
+}
+
+- (void)attachLongTapHandler
+{
+    [self setUserInteractionEnabled:YES];
+    UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongTap:)];
+    [self addGestureRecognizer:longPress];
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self attachLongTapHandler];
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    [self attachLongTapHandler];
+}
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self attachLongTapHandler];
+    }
+    return self;
+}
+
+
+- (void)handleLongTap:(UILongPressGestureRecognizer *)recognizer
+{
+    [self becomeFirstResponder];
+    UIMenuItem *copyMenuItem = [[UIMenuItem alloc] initWithTitle:@"复制" action:@selector(copyText:)];
+    [[UIMenuController sharedMenuController] setMenuItems:@[copyMenuItem]];
+    [[UIMenuController sharedMenuController] setTargetRect:self.frame inView:self.superview];
+    [[UIMenuController sharedMenuController] setMenuVisible:YES animated:YES];
+}
+
+@end
